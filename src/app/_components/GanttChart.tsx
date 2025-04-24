0a3deeb4dd677625fc9a711bd3663c75b8252659
knu9910 <knu9910@naver.com>
2025-04-24 11:08:04 +0900

    css: style 임시 저장

--- src/app/_components/GanttChart.tsx
+++ src/app/_components/GanttChart.tsx
@@ -9,28 +9,24 @@ const tasks = [
     name: "프로젝트 기획",
     start: "2025-01-01",
     end: "2025-01-05",
-    progress: 30,
   },
   {
     id: "Task 2",
     name: "UI/UX 디자인",
     start: "2025-01-03",
     end: "2025-01-10",
-    progress: 50,
   },
   {
     id: "Task 3",
     name: "프론트엔드 개발",
     start: "2025-01-08",
     end: "2025-01-20",
-    progress: 20,
   },
   {
     id: "Task 4",
     name: "백엔드 개발",
     start: "2025-01-15",
-    end: "2025-12-31",
-    progress: 10,
+    end: "2025-3-31",
   },
 ];
 
@@ -45,8 +41,7 @@ export default function GanttChart() {
       view_mode: "Day",
       date_format: "YYYY-MM-DD",
       language: "ko",
-      lines: "vertical",
-
+      column_width: 30, // 컬럼 너비
       // view_end: "2025-12-31",
       on_click: (task) => {
         console.log(task);
